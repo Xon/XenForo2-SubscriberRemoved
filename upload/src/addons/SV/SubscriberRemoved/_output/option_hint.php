6ee041ae7eb08758789dbe52a374861053410cdc
Xon <635541+Xon@users.noreply.github.com>
2024-12-13 12:55:30 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/SubscriberRemoved/_output/option_hint.php
@@ -0,0 +1,19 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property array{enable: int, starterId: int, recipientIds: array}|null $sv_subscriberremoved_conversation_data Start conversation
+ * @property array{enable: int, nodeId: string, threadAuthorId: int}|null $sv_subscriberremoved_thread_data Create thread
+ */
+class Options
+{
+}
